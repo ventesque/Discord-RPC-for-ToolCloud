afbe13c08dec588fafe15e581f4486b28d0de82b
ventesque <vilevari@gmail.com>
2025-09-09 14:30:09 +0200

    Switched api to xhayper's api to allow for start/end timestamp

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const WebSocket = require("ws");
-const RPC = require("discord-rpc");
+import { WebSocket } from "ws";
+import { Client} from "@xhayper/discord-rpc";
 // put your own application id in here
-const clientId = ("");
+const clientId = ("1412869654262710443");
 
 let retryInterval;
 let wss;
@@ -20,7 +20,7 @@ function startRetryLoop() {
 function tryLogin() {
     // creating new rpc client because if the connection fails
     // you cant retry with the previous one
-    const rpc = new RPC.Client({ transport: 'ipc' });
+    const rpc = new Client({ clientId });
 
     rpc.login({ clientId }).catch(err => {
         console.error("Connection failed:", err.message);
@@ -59,7 +59,7 @@ function tryLogin() {
 
                 // if player is paused clear the rich presence
                 if (!inactive && (track.position === lastMessage.position)) {
-                    rpc.clearActivity();
+                    rpc.user?.clearActivity();
                     lastMessage = track;
                     inactive = true;
                     console.log("Player is inactive - activiy cleared");
@@ -74,34 +74,13 @@ function tryLogin() {
                 if (inactive) return;
                 lastMessage = track;
 
-                // this is the presence shown on discord
-                // some of these attributes are being limited by discord so they dont show
-                // eg. type, buttons, endTimestamp
-                // there are more attributes that you can use that I didnt include in here
-                const presence = {
-                    details: track.title,
-                    state: "by " + track.artist,
-                    startTimestamp: Date.now() - track.position,
-                    endTimestamp: Date.now() - track.position + track.duration,
-                    // you can override the application image with this line
-                    // although you need to upload a new image to your discord application with
-                    // the name set here anyway so at that point you could just change
-                    // the application image instead
-                    //largeImageKey: "soundcloud-icon", 
-                    type: 2,
-                    buttons: [
-                        { label: "Listen Now", url: track.songLink },
-                    ]
-                }
-
-                rpc.setActivity(presence);
-                
+                updatePresence(track, rpc);
             });
 
             // clear the rich presence upon closing browser/soundcloud tab
             ws.on("close", () => {
                 console.log("ToolCloud disconnected");
-                rpc.clearActivity();
+                rpc.user?.clearActivity();
             });
         });
     });
@@ -143,6 +122,23 @@ function shutdownServer() {
         // in this if statement
         wss = null;
     }
+}
+
+function updatePresence(track, rpc) {
+    // this is the presence shown on discord
+    const presence = {
+        details: track.title,
+        state: "by " + track.artist,
+        startTimestamp: Date.now() - track.position,
+        endTimestamp: Date.now() - track.position + track.duration,
+        //largeImageKey: "soundcloud-icon", 
+        type: 2,
+        buttons: [
+            { label: "Listen Now", url: track.songLink },
+        ]
+    }
+
+    rpc.user?.setActivity(presence);
 }
 
 // initialization of the script
